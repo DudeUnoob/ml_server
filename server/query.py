335d07d710c34a275322ff7535735a1cee011a6c
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2025-03-27 21:23:31 -0500

    Better classification model

--- server/query.py
+++ server/query.py
@@ -3,11 +3,17 @@ from pydantic import BaseModel,  Field, field_validator
 
 class PredictionRequest(BaseModel):
     Age: int = Field(..., ge=0, le=120, description="Patient's age")
-    Gender: int = Field(..., ge=0, le=1, description="Patient's gender (0: Female, 1: Male)")
-    BloodPressure: int = Field(..., ge=0, le=300, description="Blood pressure reading")
-    Cholesterol: int = Field(..., ge=0, le=600, description="Cholesterol level")
-    HeartRate: int = Field(..., ge=30, le=220, description="Heart rate in beats per minute")
-    #QuantumPatternFeature: float = Field(..., description="Quantum pattern feature value")
+    Sex: int = Field(..., ge=0, le=1, description="Patient's gender (0: Female, 1: Male)")
+    ChestPainType: int = Field(..., ge=0, le=4, description="Chest Pain Type")
+    RestingBP: int = Field(..., ge=0, le=600, description="Cholesterol level")
+    Cholesterol: int = Field(...,  description="Heart rate in beats per minute")
+    FastingBS: int = Field(..., ge=0, le=1)
+    RestingECG: int = Field(..., ge=0, le=2)
+    MaxHR: int = Field(..., ge=60, le=202)
+    ExerciseAngina: int = Field(..., ge=0, le=1)
+    Oldpeak: float = Field(...)
+    ST_Slope: int = Field(...,ge=0, le=2)
+ 
     
     
     @field_validator("Age")
